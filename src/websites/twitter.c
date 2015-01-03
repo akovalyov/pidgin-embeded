5c4bf2ebbd586361388dafdeaf85fc65ff057889
Andrew Kovalyov <andrew.kovalyoff@gmail.com>
2015-01-03 23:12:12 +0200

    Several typos fix

--- src/websites/twitter.c
+++ src/websites/twitter.c
@@ -1,8 +1,9 @@
 #include <websites.h>
-WebsiteInfo twitter= {
+
+WebsiteInfo twitter = {
     "twitter",
     "/^https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(es)?\/(\d+)$/",
-    'https://api.twitter.com/1.1/statuses/oembed.json?url=https://twitter.com/$2/status/$4',
+    "https://api.twitter.com/1.1/statuses/oembed.json?url=https://twitter.com/$2/status/$4",
     NULL
 };
 
