393303b0a2b4d513c7faca1e1a01529d32338c49
Andrew Kovalyov <andrew.kovalyoff@gmail.com>
2015-01-04 00:52:59 +0200

    Comment image related stuff

--- src/websites/imageview.c
+++ src/websites/imageview.c
@@ -1,27 +1,9 @@
-/*
- * =====================================================================================
- *
- *       Filename:  imageview.c
- *
- *    Description:  just for view the image file
- *
- *        Version:  1.0
- *        Created:  2010年04月05日 15时02分58秒 CST
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  lerosua (), lerosua@gmail.com
- *        Company:  Cyclone
- *
- * =====================================================================================
- */
-
 #include <websites.h>
 
 WebsiteInfo imageview= {
     "imageview",
-    "^http://.*\\.(jpg|png|gif|jpeg|xpm|bmp)$",
-    NULL,
+    "^(?<video_id>https?://.*\\.(jpg|png|gif|jpeg|xpm|bmp))$",
+    "<p><img src=\"%VIDEO_ID%\" width=\"500\" alt=\"Bild;r\"></p>",
     NULL
 };
 
